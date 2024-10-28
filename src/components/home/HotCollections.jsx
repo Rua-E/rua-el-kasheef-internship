a622941fb0614be67cb449cbe8c8a85c5b5ec231
Rua <rua.elkasheef@gmail.com>
2024-10-27 21:20:52 -0400

    implementing loading state

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -6,15 +6,15 @@ import OwlCarousel from 'react-owl-carousel';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
 
+import nftImage from "../../images/nftImage.jpg";
+import AuthorImage from "../../images/author_thumbnail.jpg";
+
 const HotCollections = () => {
-// MAPPING THE AUTHORS
-  const [authors, setAuthors] = useState([]); 
-// LOADING STATE
-  const [loading, setLoading] = useState(true);
+  const [authors, setAuthors] = useState([]); // MAPPING THE AUTHORS
+  const [loading, setLoading] = useState(true); // LOADING STATE
 
   async function fetchCollection() {
     setLoading(true);
-
     try {
       const { data } = await axios.get(`https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections`);
       console.log('Fetched data:', data);
@@ -46,43 +46,66 @@ const HotCollections = () => {
     nav: true
   };
 
-if (loading)
- {
+if (loading) {
   return (
     <section id="section-collections" className="no-bottom">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-12">
-            <div className="text-center">
-              <h2>Hot Collections</h2>
-              <div className="small-border bg-color-2"></div>
-            </div>
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-12">
+          <div className="text-center">
+            <h2>Hot Collections</h2>
+            <div className="small-border bg-color-2"></div>
           </div>
-           <div className="d-flex gap-4">
-              {[1, 2, 3, 4].map((index) => (
-                <div key={index} className="col-lg-3 col-lg-3 col-md-6 col-sm-12">
-                    <div className="nft_coll w-full">
-                      <div className="nft_wrap">
-                        <div className="skeleton-box h-64 w-full"></div>
-                      </div>
-                      <div className="nft_coll_pp">
-                        <div className="skeleton-box rounded-full h-16 w-16"></div>
-                      </div>
-                      <div className="nft_coll_info mt-3">
-                        <div className="skeleton-box h-6 w-3/4 mb-2"></div>
-                        <div className="skeleton-box h-4 w-1/2"></div>
-                      </div>
+        </div>  
+        <div className="d-flex ">
+          <OwlCarousel 
+            className='owl-carousel'
+            margin={10}
+            {...options}
+            >
+            {new Array(4).fill(0).map((index) => (
+                  <div className="item" key={index}>
+                  <div className="nft_coll w-full">
+                    <div className="nft_wrap">
+                      <Link to="/author.title">
+                        <div className="skeleton-box h-64 w-full">
+                          <img src={nftImage} className="lazy img-fluid invisible" alt="" />
+                        </div>
+                      </Link>
+                    </div>
+                    <div className="nft_coll_pp ">
+                      <Link to="/author">
+                        <div className="skeleton-box rounded-full h-16 w-16">
+                          <img className="lazy pp-coll h-16 w-16 invisible rounded-full max-w-full h-auto" src={AuthorImage} alt="" />
+                        </div>
+                      </Link>
+                      <i className="fa fa-check"></i>
+                    </div>
+                    <div className="nft_coll_info mt-3">
+                          <Link to="/explore">
+                            <div className="skeleton-box h-6 w-3/4 mb-2">
+                              <h4 className="invisible"> author.title </h4>
+                            </div>
+                            <br />
+                          </Link>
+                          <div className="skeleton-box h-4 w-1/2">
+                            <span className="invisible">ERC-code</span>
+                          </div>
                     </div>
                   </div>
-              ))}
-          </div>
-        </div>
+                </div>
+            ))}
+            </OwlCarousel>
+            </div> 
       </div>
+    </div>
+
     </section>
   );
  }
 
   return (
+    
     <section id="section-collections" className="no-bottom">
       <div className="container">
         <div className="row">
@@ -92,14 +115,14 @@ if (loading)
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-            {authors && authors.length > 0 ? (
+            {authors && authors.length > 0 && (
             <OwlCarousel 
               className='owl-theme'
               margin={10}
               {...options}
               >
               {authors.map((author, index) => (
-                    <div className="item " key={index}>
+                    <div className="item" key={index}>
                     <div className="nft_coll">
                       <div className="nft_wrap">
                         <Link to={`/${author.title}`}>
@@ -123,14 +146,17 @@ if (loading)
 
               ))}
               </OwlCarousel>
-            ) : ( 
-              <div className="col-12 text-center">
-                 <p>No collections found</p> 
-              </div>
-            )}
+            ) 
+            // : ( 
+            //   <div className="col-12 text-center">
+            //      <p>No collections found</p> 
+            //   </div>
+            // )
+          }
         </div>
       </div>
     </section>
+    
   );
 };
 
