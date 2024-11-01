638e2c53c85c02f182c5ba316f414346cbdf3648
Rua <rua.elkasheef@gmail.com>
2024-11-01 15:20:50 -0400

    implemented the loading state for the topsellers feature

--- src/components/UI/TopSeller.jsx
+++ src/components/UI/TopSeller.jsx
@@ -1,28 +1,50 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 
-const TopSellersLoading = () => {
+const TopSellersLoading = ({ topSellers }) => {
   return (
-    new Array(12).fill(0).map((_, index) => (
+    <div>
+      {topSellers?.map((topSeller, index) => (
         <li key={index}>
           <div className="author_list_pp">
-            <Link to="/author">
+            <Link to={`/author/${topSeller.authorId}`}>
               <img
                 className="lazy pp-author"
-                src={AuthorImage}
+                src={topSeller.authorImage}
                 alt=""
               />
               <i className="fa fa-check"></i>
             </Link>
           </div>
           <div className="author_list_info">
-            <Link to="/author">Monica Lucas</Link>
-            <span>2.1 ETH</span>
+            <Link to={`/author/${topSeller.authorName}`}>
+              {topSeller.authorName}
+            </Link>
+            <span>{topSeller.price} ETH</span>
           </div>
         </li>
-      ))
-  )
-}
+      ))}
+    </div>
+    // new Array(12).fill(0).map((_, index) => (
+    //     <li key={index}>
+    //       <div className="author_list_pp">
+    //         <Link to="/author">
+    //           <img
+    //             className="lazy pp-author"
+    //             src={AuthorImage}
+    //             alt=""
+    //           />
+    //           <i className="fa fa-check"></i>
+    //         </Link>
+    //       </div>
+    //       <div className="author_list_info">
+    //         <Link to="/author">Monica Lucas</Link>
+    //         <span>2.1 ETH</span>
+    //       </div>
+    //     </li>
+    //   ))
+  );
+};
 
-export default TopSellersLoading
+export default TopSellersLoading;
--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TopSeller from "../UI/TopSeller";
 import TopSellersLoading from "../UI/TopSellersLoading";
+import axios from "axios";
 
 const TopSellers = () => {
+  const [topSellers, setTopSellers] = useState([]);
+  const [isTopSellersLoading, setTopSellersLoading] = useState(true)
+
+  async function fetchTopSellers() {
+    setTopSellersLoading(true);
+    try {
+    const { data } = await axios.get('https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers');
+    setTopSellers(data);
+    } catch (error) {
+        console.error('Error fetching, sellers:', error)
+    } finally {
+      setTopSellersLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchTopSellers()
+  }, []);
+
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
@@ -15,8 +35,10 @@ const TopSellers = () => {
           </div>
           <div className="col-md-12">
             <ol className="author_list">
-              <TopSellersLoading />
-             <TopSeller />
+              {isTopSellersLoading ?
+              (<TopSellersLoading />) :
+              (<TopSeller topSellers={topSellers}/>)
+            }
             </ol>
           </div>
         </div>
