a849e9c1664f14474f3a94a6b40247e31d49c64a
Rua <rua.elkasheef@gmail.com>
2024-11-02 16:11:32 -0400

    checking main

--- src/components/UI/TopSeller.jsx
+++ src/components/UI/TopSeller.jsx
@@ -26,24 +26,6 @@ const TopSellersLoading = ({ topSellers }) => {
         </li>
       ))}
     </div>
-    // new Array(12).fill(0).map((_, index) => (
-    //     <li key={index}>
-    //       <div className="author_list_pp">
-    //         <Link to="/author">
-    //           <img
-    //             className="lazy pp-author"
-    //             src={AuthorImage}
-    //             alt=""
-    //           />
-    //           <i className="fa fa-check"></i>
-    //         </Link>
-    //       </div>
-    //       <div className="author_list_info">
-    //         <Link to="/author">Monica Lucas</Link>
-    //         <span>2.1 ETH</span>
-    //       </div>
-    //     </li>
-    //   ))
   );
 };
 
