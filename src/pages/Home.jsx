22712a255e8e3380b89d6c42388bf6adcfe6cf5f
Rua <rua.elkasheef@gmail.com>
2024-11-08 10:16:20 -0500

    fixing refresh settings

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,34 +7,6 @@ import NewItems from "../components/home/NewItems";
 import TopSellers from "../components/home/TopSellers";
 
 const Home = () => {
- 
-  useEffect(() => { 
-    // TO SAVE THE SCROLL POSITION
-    const saveScrollPosition = () => {
-      sessionStorage.setItem('scrollPosition', window.scrollY);
-    };
-    // RESTORE THE SCROLL POSITION AND SAVE
-    const restoreScrollPosition = () => {
-      const scrollPosition = sessionStorage.getItem('scrollPosition');
-      if (scrollPosition) {
-        window.scrollTo(scrollPosition, parseInt(scrollPosition, 10));
-      } 
-    };
-
-    restoreScrollPosition();
-
-    window.addEventListener('beforeunload', saveScrollPosition);
-
-    return () => {
-      window.removeEventListener('beforeunload', saveScrollPosition);
-    };
-
-  }, []);
-
-  // useEffect(() => {
-  //   window.scrollTo(0, 0);
-  // }, []);
-
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
